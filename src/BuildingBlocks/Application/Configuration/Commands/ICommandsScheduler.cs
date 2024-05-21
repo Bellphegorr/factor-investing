5633dc64ad717d5738d862d59d3b5513f696ddbd
José Ronaldo de Oliveira Júnior <=>
2024-05-20 22:42:25 -0300

    command classes configurations

---
+++ src/BuildingBlocks/Application/Configuration/Commands/ICommandsScheduler.cs
@@ -0,0 +1,10 @@
+﻿using FactorInvesting.BuildingBlocks.Application.Contracts;
+
+namespace FactorInvesting.BuildingBlocks.Application.Configuration.Commands;
+
+public interface ICommandsScheduler
+{
+    Task EnqueueAsync(ICommand command);
+
+    Task EnqueueAsync<T>(ICommand<T> command);
+}
---
+++ src/BuildingBlocks/Application/Configuration/Commands/InternalCommandBase.cs
@@ -0,0 +1,23 @@
+﻿using FactorInvesting.BuildingBlocks.Application.Contracts;
+
+namespace FactorInvesting.BuildingBlocks.Application.Configuration.Commands;
+
+public abstract class InternalCommandBase(Guid id) : ICommand
+{
+    public Guid Id { get; } = id;
+}
+
+public abstract class InternalCommandBase<TResult> : ICommand<TResult>
+{
+    protected InternalCommandBase()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    protected InternalCommandBase(Guid id)
+    {
+        Id = id;
+    }
+
+    public Guid Id { get; }
+}
