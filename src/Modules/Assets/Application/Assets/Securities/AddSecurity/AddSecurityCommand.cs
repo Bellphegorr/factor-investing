0366cd0d58a278f4f6c32c9b6cf5d840b8524a9e
José Ronaldo de Oliveira Júnior <=>
2024-05-27 22:49:48 -0300

    starting to create add security command

---
+++ src/Modules/Assets/Application/Assets/Securities/AddSecurity/AddSecurityCommand.cs
@@ -0,0 +1,11 @@
+using FactorInvesting.BuildingBlocks.Application.Contracts;
+using FactorInvesting.Modules.Assets.Domain.Securities;
+
+namespace FactorInvesting.Modules.Assets.Application.Assets.Securities.AddSecurity;
+
+public class AddSecurityCommand : ICommand
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public SecurityTypes Type { get; set; }
+}
---
+++ src/Modules/Assets/Application/Assets/Securities/AddSecurity/AddSecurityCommandHandler.cs
@@ -0,0 +1,16 @@
+using CompanyName.MyMeetings.Modules.Administration.Application.Configuration.Commands;
+using FactorInvesting.Modules.Assets.Domain.Securities;
+
+namespace FactorInvesting.Modules.Assets.Application.Assets.Securities.AddSecurity;
+
+internal class AddSecurityCommandHandler(ISecurityRepository securityRepository)
+    : ICommandHandler<AddSecurityCommand>
+{
+    private readonly ISecurityRepository _securityRepository = securityRepository;
+
+    public async Task Handle(AddSecurityCommand request, CancellationToken cancellationToken)
+    {
+        var security = Security.Create(request.Id, request.Name, request.Type);
+        await _securityRepository.AddAsync(security);
+    }
+}
