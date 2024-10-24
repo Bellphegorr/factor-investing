af313b2c83eecb2bc1ca91690984e24332e4d793
José Ronaldo de Oliveira Júnior <=>
2024-10-24 13:48:34 -0300

    update Entity Framework Core and Npgsql package versions; add Security migration and model snapshot for Assets module

---
+++ src/Modules/Assets/Infrastructure/Migrations/20241024163135_Security.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FactorInvesting.Modules.Assets.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Security : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "assets");
+
+            migrationBuilder.CreateTable(
+                name: "Securities",
+                schema: "assets",
+                columns: table => new
+                {
+                    SecurityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SecurityName = table.Column<string>(type: "text", nullable: false),
+                    SecurityType = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Securities", x => x.SecurityId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Securities",
+                schema: "assets");
+        }
+    }
+}
